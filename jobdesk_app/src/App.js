d0884e614f6d1fec7b33c7012067ce664f83f5fb
Nikita Perkovskiy <nikitaperkovskij@gmail.com>
2023-04-27 12:58:46 +0200

    instal react appp, config inviroment

---
+++ jobdesk_app/src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+import { MantineProvider, Text } from "@mantine/core";
+
+const App = () => {
+  return <MantineProvider withGlobalStyles withNormalizeCSS></MantineProvider>;
+};
+export default App;
