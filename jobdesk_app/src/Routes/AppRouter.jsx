27c0a8d544f1fe3a5b61b92abddf1590be7e1456
Nikita Perkovskiy <nikitaperkovskij@gmail.com>
2023-05-25 22:34:01 +0200

    add hashRouter

--- jobdesk_app/src/Routes/AppRouter.jsx
+++ jobdesk_app/src/Routes/AppRouter.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "../Pages/MainPage/MainPage";
 import FavoritePage from "../Pages/FavoritePage/FavoritePage";
 import VacancyPage from "../Pages/VacancyPage/VacancyPage";
